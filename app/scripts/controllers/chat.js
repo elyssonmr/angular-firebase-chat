235490f87752e0de5a713aaad040a795ace1a303
Elysson MR <elyssonmr@gmail.com>
2014-04-23 11:07:37 -0300

    Improvements in chat and messages load

--- app/scripts/controllers/chat.js
+++ app/scripts/controllers/chat.js
@@ -2,6 +2,14 @@ angular.module('angularFirebaseChatApp')
     .controller('ChatController', function ($scope, $firebase) {
         var messagesRef = new Firebase("https://amber-fire-1008.firebaseio.com/messages");
         $scope.messages = $firebase(messagesRef);
+        $('#loading').modal('show');
+        $scope.messages.$on("loaded", function() {
+            $('#loading').modal('hide');
+        });
+        $scope.messages.$on("child_added", function(childSnapshot, prevChildName) {
+            var list = $("ul.list-group");
+            list.scrollTop(list.scrollTop() + 400);
+        });
         $scope.message = "";
         $scope.name = "";
         
